a54c2322bb774baf11ddd42cedd4c7fc8ca50e26
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-10-13 09:42:58 -0300

    feat: aula ao vivo - implementando testes model

--- src/test/java/com/example/testingjava/aula1/aulaAoVivo/aula03/model/ContaCorrenteTest.java
+++ src/test/java/com/example/testingjava/aula1/aulaAoVivo/aula03/model/ContaCorrenteTest.java
@@ -3,8 +3,11 @@ package com.example.testingjava.aula1.aulaAoVivo.aula03.model;
 import com.example.testingjava.aula1.aulaAoVivo.aula03.exception.InvalidNumberException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ContaCorrenteTest {
 
@@ -21,8 +24,43 @@ class ContaCorrenteTest {
         double valorSaque = 50;
         contaCorrente.depositar(valorDeposito);
 
-        contaCorrente.sacar(valorSaque);
+        boolean sucesso = contaCorrente.sacar(valorSaque);
 
         assertThat(contaCorrente.getSaldo()).isEqualTo(valorDeposito-valorSaque);
+        assertThat(sucesso).isTrue();
     }
+
+    @ParameterizedTest
+    @CsvSource({"100,50", "50,50", "10,10"})
+    void sacar2_retorneTrue_quantoHaSaldoSuficiente(double valorDeposito, double valorSaque) throws InvalidNumberException {
+        contaCorrente.depositar(valorDeposito);
+
+        boolean sucesso = contaCorrente.sacar(valorSaque);
+
+        assertThat(contaCorrente.getSaldo()).isEqualTo(valorDeposito-valorSaque);
+        assertThat(sucesso).isTrue();
+    }
+
+    @Test
+    void sacar_returnoFalse_quandoSaldoInsuficiente() throws InvalidNumberException {
+        double valorSaque = 100;
+        boolean sucesso = contaCorrente.sacar(valorSaque);
+
+        assertThat(contaCorrente.getSaldo()).isZero();
+        assertThat(sucesso).isFalse();
+    }
+
+    @Test
+    void sacar_lancarInvalidNumberException_quandoValorInvalido() {
+        double valorSaque = -100;
+
+        assertThatThrownBy(() -> contaCorrente.sacar(valorSaque))
+                .isInstanceOf(InvalidNumberException.class);
+    }
+
+//    @Test
+//    void toString_test() {
+//        contaCorrente.toString();
+//        assertThat("True").isTrue();
+//    }
 }
